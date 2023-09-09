8457f8ae9c161b7afc4b588015934c40a77f1f41
Atagher James <atagherjames@gmail.com>
2023-09-08 18:31:45 -0700

    third commmit

--- src/SocialMedia.jsx
+++ src/SocialMedia.jsx
@@ -1,15 +1,14 @@
-import { FaLinkedDn } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-
+import { FaInstagram, FaLinkedin } from "react-icons/fa";
+import "./socialmedia.css";
 import React from "react";
 
 const SocialMedia = () => {
   return (
     <div className="socialContainer">
       <a href="www.linkedin.com/in/atagher-james-217714265" target="_blank">
-        <FaLinkedDn />
+        <FaLinkedin />
       </a>
-      <a href="" target="_blank">
+      <a href="https://www.instagram.com/ataghejames/" target="_blank">
         <FaInstagram />
       </a>
     </div>
